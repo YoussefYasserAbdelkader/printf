e8c69b4e5c1c824a57fbf690e40ecce13348523b
Prince Lumor <142892127+Etornam3@users.noreply.github.com>
2024-01-29 15:52:57 -0800

    Update functions1.c

--- functions1.c
+++ functions1.c
@@ -4,6 +4,7 @@
 /**
  * print_unsigned - Prints an unsigned number
  * @types: List a of arguments
+ * a printf project for us
  * @buffer: Buffer array to handle print
  * @flags:  Calculates active flags
  * @width: get width
